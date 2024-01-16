666ad60171c17d0a85302edc6cc20dd1b2e44a73
BhargaVNaiduV <138513686+BhargaVNaiduV@users.noreply.github.com>
2024-01-16 16:20:42 -0500

    Update CI.py

--- CI.py
+++ CI.py
@@ -15,7 +15,7 @@ def main():
         if oldcommit != '' and commit != oldcommit:
             print('***** NEW COMMIT ******')
             try:
-                run_tests("this","")
+                #run_tests("this","")
                 build_docker_image()
                 break
             except Exception as e:
